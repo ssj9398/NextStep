bdbb6c3d632412ac28af4e6fa10584675f745b0a
alisyabob <sjs939883@gmail.com>
2022-08-28 22:48:02 +0900

    substring() - "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.

--- src/test/java/com/example/nextstep/StringTest.java
+++ src/test/java/com/example/nextstep/StringTest.java
@@ -1,5 +1,6 @@
 package com.example.nextstep;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -16,18 +17,29 @@ public class StringTest {
 
     @Test
     void contains(){
-        String[] actual2 = "1,2".split(",");
-        System.out.println(Arrays.toString(actual2));
+        String[] actual = "1,2".split(",");
+        System.out.println(Arrays.toString(actual));
 
-        assertThat(actual2).contains("1");
-        assertThat(actual2).contains("2");
+        assertThat(actual).contains("1");
+        assertThat(actual).contains("2");
     }
 
     @Test
     void containsExactly(){
-        String[] actual3 = "1".split(",");
-        System.out.println(Arrays.toString(actual3));
+        String[] actual = "1".split(",");
+        System.out.println(Arrays.toString(actual));
 
-        assertThat(actual3).containsExactly("1");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("substring() 메소드를 활용해 ()을 제거하고 1,2을 반환하도록 구현")
+    void subString(){
+        String actual =  "(1,2)";
+        int idx = actual.indexOf(')');
+        String substring = actual.substring(1, idx);
+        System.out.println("substring = " + substring);
+
+        assertThat(substring).isEqualTo("1,2");
     }
 }

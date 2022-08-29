5e4fda44c9b3cd53d4e678767ad58a3b6f03c8d1
alisyabob <sjs939883@gmail.com>
2022-08-30 01:11:04 +0900

    문자열 계산기 - 사용자가 입력한 문자열 값에 따라 사칙연산을 수행할 수 있는 계산기를 구현해야 한다. - 문자열 계산기는 사칙연산의 계산 우선순위가 아닌 입력 값에 따라 계산 순서가 결정된다. 즉, 수학에서는 곱셈, 나눗셈이 덧셈, 뺄셈 보다 먼저 계산해야 하지만 이를 무시한다. - 예를 들어 "2 + 3 * 4 / 2"와 같은 문자열을 입력할 경우 2 + 3 * 4 / 2 실행 결과인 10을 출력해야 한다.

--- src/test/java/com/example/nextstep/SetTest.java
+++ src/test/java/com/example/nextstep/SetTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -49,5 +50,34 @@ public class SetTest {
         boolean actualNum = numbers.contains(Integer.parseInt(input));
         System.out.println(actualNum);
         assertEquals(Boolean.parseBoolean(expected), actualNum);
+    }
+
+    @Test
+    @DisplayName("사칙연산 구현")
+    void arithmeticOperation(){
+
+        //given
+        String actual = "2 + 3 * 4 / 2";
+
+        //when
+        String[] value = actual.replace(" ","").split("");
+        double sum = Double.parseDouble(value[0]);
+        for (int i=1; i<value.length; i+=2) {
+            sum = calculateOnce(sum, value[i], Double.parseDouble(value[i+1]));
+        }
+
+        //then
+        assertThat(sum).isEqualTo(10.0);
+    }
+
+    private double calculateOnce(double sum, String s, double parseDouble) {
+        return switch (s) {
+            case "+" -> sum + parseDouble;
+            case "-" -> sum - parseDouble;
+            case "*" -> sum * parseDouble;
+            case "/" -> sum / parseDouble;
+            default -> 0.0;
+        };
+
     }
 }

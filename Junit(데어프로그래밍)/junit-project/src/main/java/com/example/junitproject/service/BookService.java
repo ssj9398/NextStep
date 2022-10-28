b17e8f77f3a149197f94b0431210f909c8980214
alisyabob <sjs939883@gmail.com>
2022-10-29 01:06:37 +0900

    [FEAT] 서비스레이어 책등록 완료

--- Junit(데어프로그래밍)/junit-project/src/main/java/com/example/junitproject/service/BookService.java
+++ Junit(데어프로그래밍)/junit-project/src/main/java/com/example/junitproject/service/BookService.java
@@ -1,7 +1,30 @@
 package com.example.junitproject.service;
 
+import com.example.junitproject.domain.Book;
+import com.example.junitproject.domain.BookRepository;
+import com.example.junitproject.web.dto.BookResponseDto;
+import com.example.junitproject.web.dto.BookSaveReqDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class BookService {
+    private final BookRepository bookRepository;
+
+    //1. 책 등록
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookResponseDto 책등록하기(BookSaveReqDto dto){
+        Book bookEntity = bookRepository.save(dto.toEntity());
+        return new BookResponseDto().toDto(bookEntity);
+    }
+
+    //2. 책목록보기
+
+    //3. 책한건보기
+
+    //4. 책삭제
+
+    //5. 책수정
 }
--- Junit(데어프로그래밍)/junit-project/src/main/java/com/example/junitproject/web/dto/BookResponseDto.java
+++ Junit(데어프로그래밍)/junit-project/src/main/java/com/example/junitproject/web/dto/BookResponseDto.java
@@ -1,4 +1,20 @@
 package com.example.junitproject.web.dto;
 
+import com.example.junitproject.domain.Book;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class BookResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+
+    public BookResponseDto toDto(Book book){
+        this.id = book.getId();
+        this.title = book.getTitle();
+        this.author = book.getAuthor();
+        return this;
+    }
 }

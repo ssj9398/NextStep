4169ce434079ab8a021bf8760e57e726d6fe86fb
alisyabob <sjs939883@gmail.com>
2022-10-31 19:30:09 +0900

    [FEAT] 서비스레이어 테스트 - 책목록보기 테스트

--- Junit(데어프로그래밍)/junit-project/src/test/java/com/example/junitproject/service/BookServiceTest.java
+++ Junit(데어프로그래밍)/junit-project/src/test/java/com/example/junitproject/service/BookServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.junitproject.service;
 
+import com.example.junitproject.domain.Book;
 import com.example.junitproject.domain.BookRepository;
 import com.example.junitproject.util.MailSender;
 import com.example.junitproject.util.MailSenderStub;
@@ -13,6 +14,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -48,6 +52,27 @@ class BookServiceTest {
         //then
         assertThat(bookResponseDto.getTitle()).isEqualTo(dto.getTitle());
         assertThat(bookResponseDto.getAuthor()).isEqualTo(dto.getAuthor());
+    }
+
+    @Test
+    public void 책목록보기테스트(){
+        //given
+
+        //stub
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book(1L,"제목1","저자1"));
+        bookList.add(new Book(2L,"제목2","저자2"));
+
+        when(bookRepository.findAll()).thenReturn(bookList);
+
+        //when
+        List<BookResponseDto> dtos = bookService.책목록보기();
+
+        //then
+        assertThat(dtos.get(0).getTitle()).isEqualTo("제목1");
+        assertThat(dtos.get(0).getAuthor()).isEqualTo("저자1");
+        assertThat(dtos.get(1).getTitle()).isEqualTo("제목2");
+        assertThat(dtos.get(1).getAuthor()).isEqualTo("저자2");
     }
 
 }

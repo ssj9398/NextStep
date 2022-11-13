d27aee3f3643715169aef1aed6a2799b54b9d61c
alisyabob <sjs939883@gmail.com>
2022-11-13 17:44:41 +0900

    [TEST] 컨트롤러 레이어 테스트 - 책목록보기 완료

--- Junit(데어프로그래밍)/junit-project/src/test/java/com/example/junitproject/web/BookApiControllerTest.java
+++ Junit(데어프로그래밍)/junit-project/src/test/java/com/example/junitproject/web/BookApiControllerTest.java
@@ -1,6 +1,9 @@
 package com.example.junitproject.web;
 
 import static org.assertj.core.api.Assertions.*;
+
+import com.example.junitproject.domain.Book;
+import com.example.junitproject.domain.BookRepository;
 import com.example.junitproject.service.BookService;
 import com.example.junitproject.web.dto.request.BookSaveReqDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -9,11 +12,13 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.test.context.jdbc.Sql;
 
 //통합 테스트
 // 컨트롤러만 테스트하는게 아님
@@ -21,8 +26,10 @@ import org.springframework.http.*;
 class BookApiControllerTest {
 
     @Autowired
-
     private TestRestTemplate rt;
+
+    @Autowired
+    private BookRepository bookRepository;
 
     private static ObjectMapper objectMapper;
     private static HttpHeaders headers;
@@ -32,6 +39,37 @@ class BookApiControllerTest {
         objectMapper = new ObjectMapper();
         headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @BeforeEach //테스트 시작전 한번씩 실행
+    public void 데이터준비(){
+        String title = "제목1";
+        String author = "저자1";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+    }
+
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void getBookList_test(){
+        //given
+
+        //when
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+
+        ResponseEntity<String> response = rt.exchange("/api/v1/book", HttpMethod.GET, request, String.class);
+
+        //then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        Integer code = dc.read("$.code");
+        String title = dc.read("$.body.items[0].title");
+
+        assertThat(code).isEqualTo(1);
+        assertThat(title).isEqualTo("제목1");
+
     }
 
     @Test

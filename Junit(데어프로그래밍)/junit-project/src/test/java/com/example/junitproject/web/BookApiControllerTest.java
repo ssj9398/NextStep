8ad86bde9777e4c5ce482a912be4ec5d50560841
alisyabob <sjs939883@gmail.com>
2022-11-13 17:14:32 +0900

    [TEST] 컨트롤러 레이어 테스트 - 책저장하기

--- Junit(데어프로그래밍)/junit-project/src/test/java/com/example/junitproject/web/BookApiControllerTest.java
+++ Junit(데어프로그래밍)/junit-project/src/test/java/com/example/junitproject/web/BookApiControllerTest.java
@@ -1,7 +1,61 @@
 package com.example.junitproject.web;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+import com.example.junitproject.service.BookService;
+import com.example.junitproject.web.dto.request.BookSaveReqDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 
+//통합 테스트
+// 컨트롤러만 테스트하는게 아님
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BookApiControllerTest {
+    @Autowired
+    private BookService bookService;
 
+    @Autowired
+
+    private TestRestTemplate rt;
+
+    private static ObjectMapper objectMapper;
+    private static HttpHeaders headers;
+
+    @BeforeAll
+    public static void init(){
+        objectMapper = new ObjectMapper();
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Test
+    public void saveBook_test() throws JsonProcessingException {
+        //given
+        BookSaveReqDto bookSaveReqDto = new BookSaveReqDto();
+        bookSaveReqDto.setTitle("제목1");
+        bookSaveReqDto.setAuthor("저자1");
+
+        String body = objectMapper.writeValueAsString(bookSaveReqDto);
+
+        //when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = rt.exchange("/api/v1/book", HttpMethod.POST, request, String.class);
+
+        //then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        String title= dc.read("$.body.title");
+        String author = dc.read("$.body.author");
+
+        assertThat(title).isEqualTo("제목1");
+        assertThat(author).isEqualTo("저자1");
+    }
 }
